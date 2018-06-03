c49d9e337af3d2257cb5ee029f68577864ce0a7a
互联网部-前端-许增强 <2068214540@qq.com>
2018-06-03 11:28:19 +0800

    重要更新,引入koahub-handlebars依赖,组件分离

--- config/handlebars.js
+++ config/handlebars.js
@@ -4,21 +4,21 @@
  * @author xuzengqiang <253948113@qq.com>
  * @date 2018-05-30 12:00:47
  * @version 1.0.0
+ * 
+ * @update xuzengqiang
+ * @date 2018-06-03 11:08:50
+ * @version 1.0.1
+ * @description 采用koahub-handlebars中间件
  */
+const hbs = require('koahub-handlebars')
+const rootPath = process.cwd()
 // const Handlebars = require('handlebars')
 // const HandlebarsDelimiters = require('handlebars-delimiters')
 
 // HandlebarsDelimiters(Handlebars, ['${', '}'])
+const HandlebarsViews = () => hbs.middleware({
+    viewPath: `${rootPath}/views`,
+    partialsPath: `${rootPath}/views/components`
+})
 
-module.exports = {
-    map: {
-        hbs: 'handlebars'
-    },
-    extension: 'hbs'
-    // ,
-    // options: {
-    //     partials: {
-    //         header: 'components/header'
-    //     }
-    // }
-}
+module.exports = HandlebarsViews
