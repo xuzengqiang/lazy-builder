633abc8a9e553c1be2afd069bd017b0a0c8a2bad
xuzengqiang <253948113@qq.com>
2018-06-06 01:20:17 +0800

    删除三栏的布局方式，优化新增/编辑页组件生成功能，优化代码

--- server/controllers/AddModifyController.js
+++ server/controllers/AddModifyController.js
@@ -12,223 +12,225 @@ const logger = log4js.getLogger('index')
 const lodash = require('lodash')
 
 class AddModifyController {
-    /**
-     * 构造函数
-     * @param {Object} model - 请求的参数信息
-     * @param {Object} menu - 菜单配置
-     */
-    constructor(model, menu) {
-        this.model = model
-        this.menu = menu
+  /**
+   * 构造函数
+   * @param {Object} model - 请求的参数信息
+   * @param {Object} menu - 菜单配置
+   */
+  constructor(model, menu) {
+    this.model = model
+    this.menu = menu
 
-        /**
-         * 是否有弹出窗
-         */
-        this.hasDialog = true
+    /**
+     * 是否有弹出窗
+     */
+    this.hasDialog = true
+  }
+
+  /**
+   * 构建
+   */
+  builder() {
+    try {
+      this._createAddIndexFile()
+      this._createModifyIndexFile()
+      this._createFormFieldsFiles()
+      this._createRulesFile()
+      this._createModelFile()
+      this._createAddModifyFile()
+      this._createModifyDetailFile()
+    } catch (e) {
+      console.error(e)
     }
+  }
 
-    /**
-     * 构建
-     */
-    builder () {
-        try {
-            this._createAddIndexFile()
-            this._createModifyIndexFile()
-            this._createFormFieldsFiles()
-            this._createRulesFile()
-            this._createModelFile()
-            this._createAddModifyFile()
-            this._createModifyDetailFile()
-        } catch (e) {
-            console.error(e)
-        }
-    }
+  /**
+   * 生成入口文件
+   */
+  _createAddIndexFile() {
+    console.error('构建新增页入口文件')
+    const file = FileUtils.createFile(`${rootPath}/build/add/index.vue`)
+    FileUtils.createFile(`${rootPath}/build/add/${this.menu.name}新增.md`)
+    const template = new Template('addIndex')
+    template.compile(file, {
+      hasDialog: this.hasDialog,
+      columns: this.model.columns
+    })
+  }
 
-    /**
-     * 生成入口文件
-     */
-    _createAddIndexFile () {
-        console.error('构建新增页入口文件')
-        const file = FileUtils.createFile(`${rootPath}/build/add/index.vue`)
-        FileUtils.createFile(`${rootPath}/build/add/${this.menu.name}新增.md`)
-        const template = new Template('addIndex')
-        template.compile(file, {
-            hasDialog: this.hasDialog,
-            columns: this.model.columns
+  /**
+   * 生成入口文件
+   */
+  _createModifyIndexFile() {
+    console.error('构建编辑页入口文件')
+    const file = FileUtils.createFile(`${rootPath}/build/modify/index.vue`)
+    FileUtils.createFile(`${rootPath}/build/modify/${this.menu.name}修改.md`)
+    const template = new Template('modifyIndex')
+    template.compile(file, {
+      hasDialog: this.hasDialog,
+      columns: this.model.columns
+    })
+  }
+
+  /**
+   * 创建form-fields配置文件
+   */
+  _createFormFieldsFiles() {
+    console.error('构建字段配置文件...')
+    const columns = this.model.columns
+    let childrens
+    columns.forEach(column => {
+      childrens = column.childrens
+      Array.isArray(childrens) &&
+        childrens.forEach(children => {
+          this._createSingleFormFieldsFile(children)
         })
-    }
+    })
+  }
 
-    /**
-     * 生成入口文件
-     */
-    _createModifyIndexFile () {
-        console.error('构建编辑页入口文件')
-        const file = FileUtils.createFile(`${rootPath}/build/modify/index.vue`)
-        FileUtils.createFile(`${rootPath}/build/modify/${this.menu.name}修改.md`)
-        const template = new Template('modifyIndex')
-        template.compile(file, {
-            hasDialog: this.hasDialog,
-            columns: this.model.columns
-        })
-    }
+  /**
+   * 创建验证规则配置文件
+   */
+  _createRulesFile() {
+    console.error('创建rules.js配置文件')
+    const file = FileUtils.createFile(`${rootPath}/build/config/rules.js`)
+    const template = new Template('configRules')
+    template.compile(file)
+  }
 
-    /**
-     * 创建form-fields配置文件
-     */
-    _createFormFieldsFiles () {
-        console.error('构建字段配置文件...')
-        const columns = this.model.columns
-        columns.forEach(column => {
-            this._createSingleFormFieldsFile(column.leftcontent)
-            this._createSingleFormFieldsFile(column.main)
-            this._createSingleFormFieldsFile(column.rightcontent)
-        })
-    }
+  /**
+   * 创建model文件
+   */
+  _createModelFile() {
+    console.error('创建model.js配置文件')
+    const file = FileUtils.createFile(`${rootPath}/build/config/model.js`)
+    const template = new Template('configModel')
 
-    /**
-     * 创建验证规则配置文件
-     */
-    _createRulesFile () {
-        console.error('创建rules.js配置文件')
-        const file = FileUtils.createFile(`${rootPath}/build/config/rules.js`)
-        const template = new Template('configRules')
-        template.compile(file)
-    }
+    const obj = {}
+    const emptyKeys = []
 
-    /**
-     * 创建model文件
-     */
-    _createModelFile () {
-        console.error('创建model.js配置文件')
-        const file = FileUtils.createFile(`${rootPath}/build/config/model.js`)
-        const template = new Template('configModel')
-
-        const obj = {}
-        const emptyKeys = []
-
-        const setObj = subcolumn => {
-            if (subcolumn && Array.isArray(subcolumn.fields) && subcolumn.fields.length) {
-                let label
-                let key
-                subcolumn.fields.forEach(field => {
-                    label = field.label ? (field.label + '').trim() : ''
-                    key = field.key ? (field.key + '').trim() : ''
-                    if (label) {
-                        if (key) {
-                            lodash.set(obj, key, {
-                                isleaf: true,
-                                label
-                            })
-                        } else {
-                            emptyKeys.push({
-                                label: label,
-                                key: ''
-                            })
-                        }
-                    }
-                })
+    const setObj = subcolumn => {
+      if (subcolumn && Array.isArray(subcolumn.fields) && subcolumn.fields.length) {
+        let label
+        let key
+        subcolumn.fields.forEach(field => {
+          label = field.label ? (field.label + '').trim() : ''
+          key = field.key ? (field.key + '').trim() : ''
+          if (label) {
+            if (key) {
+              lodash.set(obj, key, {
+                isleaf: true,
+                label
+              })
+            } else {
+              emptyKeys.push({
+                label: label,
+                key: ''
+              })
             }
-        }
-
-        this.model.columns.forEach(column => {
-            setObj(column.leftcontent)
-            setObj(column.main)
-            setObj(column.rightcontent)
+          }
         })
-
-        template.compile(file, {
-            emptyKeys,
-            nodes: this.createTreeNodeList(obj)
-        })
+      }
     }
 
-    /**
-     * 开始创建add.modify配置文件
-     */
-    _createAddModifyFile () {
-        console.error('构建add.modify.js配置文件...')
-        const file = FileUtils.createFile(`${rootPath}/build/mixins/add.modify.js`)
-        const template = new Template('mixinsAddModify')
-        template.compile(file, {
-            hasDialog: this.hasDialog,
-            router: this.menu.router,
-            columns: this.model.columns
-        })
+    let childrens
+    this.model.columns.forEach(column => {
+      Array.isArray(column.childrens) && column.childrens.forEach(children => setObj(children))
+    })
+
+    template.compile(file, {
+      emptyKeys,
+      nodes: this.createTreeNodeList(obj)
+    })
+  }
+
+  /**
+   * 开始创建add.modify配置文件
+   */
+  _createAddModifyFile() {
+    console.error('构建add.modify.js配置文件...')
+    const file = FileUtils.createFile(`${rootPath}/build/mixins/add.modify.js`)
+    const template = new Template('mixinsAddModify')
+    template.compile(file, {
+      hasDialog: this.hasDialog,
+      router: this.menu.router,
+      columns: this.model.columns
+    })
+  }
+
+  /**
+   * 开始创建modify.detail配置文件
+   */
+  _createModifyDetailFile() {
+    console.error('构建modify.detail.js配置文件...')
+    const file = FileUtils.createFile(`${rootPath}/build/mixins/modify.detail.js`)
+    const template = new Template('mixinsModifyDetail')
+    template.compile(file, {
+      router: this.menu.router
+    })
+  }
+
+  /**
+   * 创建单个字段配置文件
+   */
+  _createSingleFormFieldsFile(subcolumn) {
+    const fileName = subcolumn && subcolumn.fileName ? (subcolumn.fileName + '').trim() : ''
+    if (fileName && subcolumn.fields.length) {
+      console.error(`构建字段配置文件${fileName}.js`)
+      const file = FileUtils.createFile(`${rootPath}/build/config/${fileName}.js`)
+      const template = new Template('configFormFields')
+      template.compile(file, {
+        subcolumn
+      })
+    }
+  }
+
+  /**
+   * 创建树节点
+   */
+  createTreeNodeList(fieldObject) {
+    let treeNodeList = []
+    let treeNode
+    for (let property in fieldObject) {
+      treeNode = this.getTreeNode(fieldObject, property)
+      if (treeNode) {
+        treeNodeList.push(treeNode)
+      }
+    }
+    return treeNodeList
+  }
+
+  /**
+   * 获取字段树
+   * @author xuzengqiang
+   * @date 2018-06-05 17:27:00
+   */
+  getTreeNode(fieldObject, property) {
+    let node = fieldObject[property]
+    if (!node) {
+      return null
     }
 
-    /**
-     * 开始创建modify.detail配置文件
-     */
-    _createModifyDetailFile () {
-        console.error('构建modify.detail.js配置文件...')
-        const file = FileUtils.createFile(`${rootPath}/build/mixins/modify.detail.js`)
-        const template = new Template('mixinsModifyDetail')
-        template.compile(file, {
-            router: this.menu.router
-        })
+    let treeNode = {
+      key: property,
+      childrens: []
     }
+    let subTreeNode = null
 
-    /**
-     * 创建单个字段配置文件
-     */
-    _createSingleFormFieldsFile (subcolumn) {
-        const fileName = subcolumn && subcolumn.fileName ? (subcolumn.fileName + '').trim() : ''
-        if (fileName && subcolumn.fields.length) {
-            console.error(`构建字段配置文件${fileName}.js`)
-            const file = FileUtils.createFile(`${rootPath}/build/config/${fileName}.js`)
-            const template = new Template('configFormFields')
-            template.compile(file, {
-                subcolumn
-            })
+    if (node.isleaf) {
+      treeNode.isleaf = true
+      treeNode.label = node.label
+    } else if (lodash.isPlainObject(node)) {
+      for (let property in node) {
+        subTreeNode = this.getTreeNode(node, property)
+        if (subTreeNode) {
+          treeNode.childrens.push(subTreeNode)
         }
+      }
     }
 
-    /**
-     * 创建树节点
-     */
-    createTreeNodeList (fieldObject) {
-        let treeNodeList = []
-        let treeNode
-        for (let property in fieldObject) {
-            treeNode = this.getTreeNode(fieldObject, property)
-            if (treeNode) {
-                treeNodeList.push(treeNode)
-            }
-        }
-        return treeNodeList
-    }
-
-    /**
-     * 获取字段树
-     * @author xuzengqiang
-     * @date 2018-06-05 17:27:00
-     */
-    getTreeNode (fieldObject, property) {
-        let node = fieldObject[property]
-        if (!node) {
-            return null
-        }
-
-        let treeNode = {
-            key: property,
-            childrens: []
-        }
-        let subTreeNode = null
-
-        if (node.isleaf) {
-            treeNode.isleaf = true
-            treeNode.label = node.label
-        } else if (lodash.isPlainObject(node)) {
-            for (let property in node) {
-                subTreeNode = this.getTreeNode(node, property)
-                if (subTreeNode) {
-                    treeNode.childrens.push(subTreeNode)
-                }
-            }
-        }
-
-        return treeNode
-    }
+    return treeNode
+  }
 }
 
 module.exports = AddModifyController
--- src/components/add-column-dialog/index.js
+++ src/components/add-column-dialog/index.js
@@ -3,98 +3,72 @@
  * @author: xuzengqiang
  * @date: 2018-06-02 17:49:18
  */
-; (window => {
-    const DEFAULT_COLUMN = 4
-    const ModelConfig = () => {
-        return {
-            title: '',
-            main: null,
-            leftcontent: null,
-            rightcontent: null
-        }
+;(window => {
+  const DEFAULT_COLUMN = 4
+
+  const getModel = totalspan => {
+    return {
+      title: '',
+      fields: [],
+      fileName: '',
+      fieldsConfig: '',
+      column: DEFAULT_COLUMN,
+      totalspan: totalspan
     }
+  }
 
-    const ColumnConfig = () => {
-        return {
-            main: 24,
-            leftcontent: 0,
-            rightcontent: 0
+  const AddColumnDialog = {
+    template: '#add-column-dialog-template',
+    name: 'AddColumnDialog',
+    data() {
+      return {
+        layout: 'one-column',
+        show: false,
+        column: 12,
+        title: ''
+      }
+    },
+    watch: {
+      show(value) {
+        if (value) {
+          this.title = ''
+          this.column = 12
         }
-    }
+      }
+    },
+    methods: {
+      addColumn() {
+        const column = this.column
+        let childrens = []
 
-    const getModel = totalspan => {
-        return {
-            title: '',
-            fields: [],
-            fileName: '',
-            fieldsConfig: '',
-            column: DEFAULT_COLUMN,
-            totalspan: totalspan
+        // 生成子栏目
+        switch (this.layout) {
+          case 'one-column':
+            childrens.push(getModel(24))
+            break
+          case 'two-column':
+            childrens.push(getModel(this.column))
+            childrens.push(getModel(24 - this.column))
         }
-    }
 
-    const AddColumnDialog = {
-        template: '#add-column-dialog-template',
-        name: 'AddColumnDialog',
-        data () {
-            return {
-                model: ModelConfig(),
-                layout: 'one-column',
-                show: false,
-                column: ColumnConfig()
-            }
-        },
-        watch: {
-            show (value) {
-                if (value) {
-                    this.model = ModelConfig()
-                    this.column = ColumnConfig()
-                }
-            }
-        },
-        methods: {
-            addColumn () {
-                const column = this.column
-
-                const model = {
-                    title: this.model.title.trim(),
-                    main: getModel(column.main),
-                    leftcontent: this.layout !== 'one-column' ? getModel(column.leftcontent) : null,
-                    rightcontent: this.layout === 'three-column' ? getModel(column.rightcontent) : null
-                }
-
-                this.show = false
-                this.$emit('add-column', model)
-            },
-            /**
-             * 点击布局方式之后的处理
-             * @param {String} layout - 布局方式
-             */
-            layoutClicked (layout) {
-                this.layout = layout
-                switch (layout) {
-                    case 'one-column':
-                        this.setColumn(24, 0)
-                        break
-                    case 'two-column':
-                        this.setColumn(12, 12)
-                        break
-                    case 'three-column':
-                        this.setColumn(8, 8)
-                }
-            },
-            /**
-             * 设置列数
-             * @param {Number} main - 主栏目列数
-             * @param {Number} leftcontent - 左侧栏目列数
-             */
-            setColumn (mainColumn, leftContentColumn) {
-                this.column.main = mainColumn
-                this.column.leftcontent = leftContentColumn
-                this.column.rightcontent = 24 - mainColumn - leftContentColumn
-            }
+        this.show = false
+        this.$emit('add-column', {
+          title: this.title,
+          childrens
+        })
+      },
+      /**
+       * 点击布局方式之后的处理
+       * @param {String} layout - 布局方式
+       */
+      layoutClicked(layout) {
+        this.layout = layout
+        if (layout === 'two-column') {
+          this.column = 12
         }
+      }
     }
+  }
 
-    window.AddColumnDialog = AddColumnDialog
+  window.AddColumnDialog = AddColumnDialog
 })(window)
