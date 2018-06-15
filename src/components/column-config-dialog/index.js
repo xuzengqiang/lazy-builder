6f30a08c9835e491cf0639ef1f31f8c7ede8ba0c
互联网部-前端-许增强 <2068214540@qq.com>
2018-06-15 10:36:32 +0800

    新增.editorconfig规范代码

--- src/components/column-config-dialog/index.js
+++ src/components/column-config-dialog/index.js
@@ -4,56 +4,72 @@
  * @date: 2018-06-04 16:45:59
  */
 (window => {
-    const ModelConfig = () => {
-        return {
-            title: '',
-            column: 6,
-            fileName: ''
-        }
+  const ModelConfig = () => {
+    return {
+      title: '',
+      column: 6,
+      fileName: ''
     }
-    const ColumnConfigDialog = {
-        template: '#column-config-dialog-template',
-        name: 'ColumnConfigDialog',
-        data () {
-            return {
-                model: ModelConfig(),
-                show: false,
-                rules: {
-                    fileName: [{
-                        required: true,
-                        message: '文件名称不能为空',
-                        trigger: 'blur'
-                    }]
-                }
-            }
-        },
-        /**
-         * 属性列表
-         * @property {Boolean} hasTitle - 父栏目是否存在标题
-         */
-        props: {
-            hasTitle: Boolean
-        },
-        watch: {
-            show (value) {
-                value && (this.model = ModelConfig())
-            }
-        },
-        methods: {
-            setColumn () {
-                this.$refs.form.validate(valid => {
-                    if (valid) {
-                        this.show = false
-                        this.$emit('set-column', {
-                            title: this.model.title,
-                            column: this.model.column,
-                            fileName: this.model.fileName
-                        })
-                    }
-                })
-            }
-        }
-    }
+  }
 
-    window.ColumnConfigDialog = ColumnConfigDialog
+  /**
+   * 是否数组
+   * @type {Array}
+   */
+  const yesOrNo = [
+    {
+      label: '是',
+      value: true
+    },
+    {
+      label: '否',
+      value: false
+    }
+  ]
+
+  const ColumnConfigDialog = {
+    template: '#column-config-dialog-template',
+    name: 'ColumnConfigDialog',
+    data () {
+      return {
+        model: ModelConfig(),
+        show: false,
+        rules: {
+          fileName: [{
+            required: true,
+            message: '文件名称不能为空',
+            trigger: 'blur'
+          }]
+        }
+      }
+    },
+    /**
+     * 属性列表
+     * @property {Boolean} hasTitle - 父栏目是否存在标题
+     */
+    props: {
+      hasTitle: Boolean
+    },
+    watch: {
+      show (value) {
+        value && (this.model = ModelConfig())
+      }
+    },
+    methods: {
+      setColumn () {
+        this.$refs.form.validate(valid => {
+          if (valid) {
+            this.show = false
+            this.$emit('set-column', {
+              title: this.model.title,
+              column: this.model.column,
+              fileName: this.model.fileName
+            })
+          }
+        })
+      }
+    }
+  }
+
+  window.ColumnConfigDialog = ColumnConfigDialog
 })(window)
